59bb1bda029f8f652239925cf43e631d4b55c70b
nakayama91 <k.naka10ry@gmail.com>
2021-10-10 08:50:30 +0000

    [finish]ch3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '45ced52dc8d85a10d8d5b5943d334d6a0e6c5c70a708a457df620ca35cc5a94af318f3f5daa89f0a1af0d2dc5786df2bc53cbc70c88d51b7642807c907e224d7'
